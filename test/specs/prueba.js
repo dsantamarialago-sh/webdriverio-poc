97fdf824b7e5d2a38943ab87fd1dfab62af0d864
dsantamarialago-sh <dsantamaria18@gmail.com>
2020-12-10 17:56:58 +0100

    Modified test assertions

--- test/specs/prueba.js
+++ test/specs/prueba.js
@@ -1,10 +1,15 @@
+const { header } = require('../pageobjects/HomePage');
 const homePage = require('../pageobjects/HomePage');
 
 describe('SH home page', () => {
     it('should render required components', () => {
         homePage.open();
         homePage.isLoaded();
-
+        
+        expect(homePage.header.logo).toBeDisplayed;
+        expect(homePage.header.searchBox).toBeDisplayed;
+        expect(homePage.jumbotron).toBeDisplayed;
         expect(homePage.footer.fanProtectLogo).toBeDisplayed;
+        expect(homePage.footer.fanProtectLink).toHaveAttributeContaining('href', 'http://www.stubhub.com/promise/')
     });
 });
