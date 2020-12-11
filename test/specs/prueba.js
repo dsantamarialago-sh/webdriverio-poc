73ea0ffeb6f4fd86ef91e71a3f9fa4daf4f78620
dsantamarialago-sh <dsantamaria18@gmail.com>
2020-12-11 13:59:54 +0100

    Added multitab example test

--- test/specs/prueba.js
+++ test/specs/prueba.js
@@ -1,6 +1,8 @@
 const homePage = require('../pageobjects/HomePage');
+const loginPage = require('../pageobjects/LoginPage.js')
 
 describe('SH home page', () => {
+
     it('should render required components', () => {
         homePage.open();
         homePage.isLoaded();
@@ -10,5 +12,24 @@ describe('SH home page', () => {
         expect(homePage.jumbotron).toBeDisplayed;
         expect(homePage.footer.fanProtectLogo).toBeDisplayed;
         expect(homePage.footer.fanProtectLink).toHaveAttributeContaining('href', 'http://www.stubhub.com/promise/')
+    });
+
+    it('should open SH home page and another tab with SH Login page', () => {
+        homePage.open();
+        homePage.isLoaded();
+        //const initialTab = browser.getWindowHandle();
+
+        loginPage.openInNewTab(browser.getUrl());
+        const tabs = browser.getWindowHandles();
+        console.log(`>>> TABS: ${tabs[1]}`);
+
+        browser.switchToWindow(tabs[1]);
+
+        loginPage.isLoaded();
+        loginPage.login('api_uk_sell_buyer20@testmail.com', 'Password');
+        browser.pause(5000);
+        
+        browser.switchToWindow(tabs[0]);
+        browser.refresh();
     });
 });
