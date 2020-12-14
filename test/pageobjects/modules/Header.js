7af34c951138e843a71ffeede888225fb8321a2d
dsantamarialago-sh <dsantamaria18@gmail.com>
2020-12-14 15:01:57 +0100

    Added selector to Header module and modified the test

--- test/pageobjects/modules/Header.js
+++ test/pageobjects/modules/Header.js
@@ -5,6 +5,7 @@ class Header {
 
     get logo() {return $('a.logo__link')};
     get searchBox() {return $('div.SearchBar__view > input')};
+    get signInText() {return $('div.mh__nav-menu > a > span.mh__nav-menu-header-text.menu__header-text-sign-in')};
 
     /******************/
     /** Page actions **/
