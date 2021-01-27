dc0ed4200903752a129416e44a07a00325d7b93f
sausin <sausin@stubhub.com>
2021-01-27 16:29:59 +0100

    INTLSH-6120 add resource mock

--- test/specs/mocking.js
+++ test/specs/mocking.js
@@ -2,13 +2,20 @@ const eventsRecommendation = require('./mocks/eventsRecommendation');
 const homePage = require('../pageobjects/HomePage');
 
 describe('testing mocking feature for home', () => {
+  const mockResponse = (url, response) => {
+    const mock = browser.mock(url);
+    mock.respond(response);
+  }
   it('API responses', () => {
-    const mock = browser.mock('**/bfx/api/search/catalog/events/v3/eventGroupings**', {
-      method: 'get'
-    });
-    mock.respond(eventsRecommendation);
+    mockResponse('**/bfx/api/search/catalog/events/v3/eventGroupings**', eventsRecommendation);
     homePage.open();
     $('.genre-module--entity-list-container').waitForExist();
     browser.saveScreenshot('./screenshots/image-api-mock.png');
+  })
+
+  it('Mock resources', () => {
+    mockResponse('**/home.**.css', './test/specs/mocks/cssMock.css');
+    homePage.open();
+    browser.saveScreenshot('./screenshots/image-resource-mock.png');
   })
 });
